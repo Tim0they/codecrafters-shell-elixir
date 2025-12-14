5200534306961a7402d4716817a28a257d4f2252
Lufuno <ltshishivhiri@gmail.com>
2025-12-14 23:07:26 +0200

    stage 2

--- lib/main.ex
+++ lib/main.ex
@@ -1,6 +1,9 @@
 defmodule CLI do
-  def main(_args) do
+  def main(args) do
     # TODO: Uncomment the code below to pass the first stage
     IO.write("$ ")
+    command = IO.gets("") |> String.trim()
+    IO.puts("#{command}: command not found")
+    main(args)
   end
 end
