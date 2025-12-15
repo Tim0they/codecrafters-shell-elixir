d3024d079387cc2299cf3095277a0c7cde2349da
Lufuno <ltshishivhiri@gmail.com>
2025-12-15 19:03:17 +0200

    stage 5

--- lib/main.ex
+++ lib/main.ex
@@ -1,15 +1,33 @@
 defmodule CLI do
+
+  @commands ["exit", "echo", "type"]
+
   def main(args) do
     # TODO: Uncomment the code below to pass the first stage
     IO.write("$ ")
     command = IO.gets("") |> String.trim()
-
-    case command do
-      "exit" -> exit(:normal)
-      "echo "<> value -> IO.puts(value)
-      _ -> IO.puts("#{command}: command not found")
-    end
+    evaluate(command)
     main(args)
 
+  end
+
+  def evaluate("exit") do
+    exit(:normal)
+  end
+
+  def evaluate("echo "<> cmd) do
+    IO.puts(cmd)
+  end
+
+  def evaluate("type "<> cmd) do
+    if Enum.member?(@commands, cmd) do
+      IO.puts("#{cmd} is a shell builtin")
+    else
+      IO.puts("#{cmd}: not found")
+    end
+  end
+
+  def evaluate(cmd) do
+    IO.puts("#{cmd}: command not found")
   end
 end
